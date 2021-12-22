9bcab4abdff295b5bb10ac116accdfe81782861a
codpoe <codpoe.me@gmail.com>
2021-12-23 02:33:34 +0800

    fix: support relative demo src

--- packages/onepress/src/node/plugins/mdx/demo.ts
+++ packages/onepress/src/node/plugins/mdx/demo.ts
@@ -2,9 +2,11 @@
  * modify from vite-plugin-react-pages
  */
 import type { Root } from 'mdast';
+import path from 'path';
 import { readFile } from 'fs-extra';
 import { extract, parse, strip } from 'jest-docblock';
 import { DEMO_MODULE_ID_PREFIX, MDX_DEMO_RE } from '../../constants';
+import { slash } from '../../utils';
 
 export function getDemoModuleId(filePath: string) {
   return `${DEMO_MODULE_ID_PREFIX}${filePath}`;
@@ -15,14 +17,18 @@ export function extractDemoPath(id: string) {
 }
 
 export function demoMdxPlugin() {
-  return function demoTransformer(tree: Root) {
+  return function demoTransformer(tree: Root, file: any) {
     const addImports: string[] = [];
 
     tree.children.forEach((child: any) => {
       if ((child.type as string) === 'jsx') {
-        const [, src] = (child.value as string).match(MDX_DEMO_RE) || [];
+        let [, src] = (child.value as string).match(MDX_DEMO_RE) || [];
 
         if (src) {
+          src = src.startsWith('/')
+            ? src
+            : slash(path.resolve(file.dirname, src));
+
           const imported = `__demo_${addImports.length}`;
           addImports.push(
             `import * as ${imported} from '${getDemoModuleId(src)}';`
